8e1f3a9396d828eb69dc08c3989aee2bb891c68b
Mendes16 <33485565+Mendes16@users.noreply.github.com>
2018-06-08 11:45:03 -0300

    Add files via upload

---
+++ Projeto/conexao.php
@@ -0,0 +1,34 @@
+<?php
+
+    $conexao = mysqli_connect("localhost", "root", "root");
+    mysqli_select_db($conexao, "tcc");
+?>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Cadastro</title>
+    </head>
+    <body>
+        <?php
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $senha = $_POST["senha"];
+
+            $inserir = "INSERT INTO usuario (cod_usuario, nome, email, senha) VALUES (NULL, '$nome', '$email', '$senha');";
+            mysqli_query($conexao, $inserir) or die (mysqli_error($conexao));
+
+
+
+            $cpf = $_POST["cpf"];
+            $especificacao = $_POST["especificacao"];
+
+            $inserir2 = "INSERT INTO fisico (cpf,especificacao, cod_usuario) VALUES ('$cpf', '$especificacao', LAST_INSERT_ID());";
+        mysqli_query($conexao, $inserir2) or die (mysqli_error($conexao));
+
+        $inserir2 = "INSERT INTO juridico (cpf,especificacao, cod_usuario) VALUES ('$cpf', '$especificacao', LAST_INSERT_ID());";
+        mysqli_query($conexao, $inserir2) or die (mysqli_error($conexao));
+        echo"Você foi cadastrado com sucesso. Clique <a href='login.html'>aqui</a> para fazer log-in.";
+        ?>
+    </body>
+</html>
